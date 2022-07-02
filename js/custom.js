a09865b5256a49ac993a800434cdd208cdd77b2c
jahidulkanchan <103818133+jahidulkanchan@users.noreply.github.com>
2022-07-02 16:45:51 +0600

    adding heading

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+
+//sticky nav  
+window.addEventListener("scroll" , function(){
+  var navslide = document.getElementById("header");
+  navslide.classList.toggle("sticky" , window.scrollY >300 );
+});
+
+//nav toggle
+var navToggler = document.getElementById("nav-toggle");
+var menu = document.querySelector(".menu");
+
+navToggler.onclick = function () {
+  navToggler.classList.toggle("active");
+  menu.classList.toggle("active");
+};
+document.onclick = function (e) {
+  if (e.target.id !== "nav-toggle" && e.target.classList !== "menu") {
+    navToggler.classList.remove("active");
+    menu.classList.remove("active");
+  }
+};
