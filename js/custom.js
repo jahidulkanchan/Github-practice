dae632a1aa5f4ca9ecad98b9504f5377175d46eb
jahidulkanchan <103818133+jahidulkanchan@users.noreply.github.com>
2022-07-05 16:23:11 +0600

    new

--- js/custom.js
+++ js/custom.js
@@ -6,8 +6,49 @@ window.addEventListener("load", function(){
 //sticky nav  
 window.addEventListener("scroll" , function(){
   var navslide = document.getElementById("header");
-  navslide.classList.toggle("sticky" , window.scrollY >300 );
+  navslide.classList.toggle("sticky" , window.scrollY >200 );
 })
+//active menu
+// $(document).on('click', 'ul.menu li', function(){
+//   $(this).addClass('active').siblings().removeClass('active');
+// })
+//smoothscroll js
+$(document).ready(function () {
+  $(document).on("scroll", onScroll);
+  $('.scroll-to-section a[href^="#"]').on('click', function (e) {
+      e.preventDefault();
+      $(document).off("scroll");  
+      $('.scroll-to-section a').each(function () {
+          $(this).removeClass('active');
+      })
+      $(this).addClass('active');
+      var target = this.hash,
+      menu = target;
+      var target = $(this.hash);
+      $('html, body').stop().animate({
+          scrollTop: (target.offset().top) + 1
+      }, 100, 'swing', function () {
+          window.location.hash = target;
+          $(document).on("scroll", onScroll);
+      });
+  });
+});
+
+function onScroll(event){
+  var scrollPos = $(document).scrollTop();
+  $('.menu a').each(function () {
+      var currLink = $(this);
+      var refElement = $(currLink.attr("href"));
+      if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+          $('.menu li a').removeClass("active");
+          currLink.addClass("active");
+      }
+      else{
+          currLink.removeClass("active");
+      }
+  });
+}
+
 //nav toggle
 var navToggler = document.getElementById("nav-toggle");
 var menu = document.querySelector(".menu");
